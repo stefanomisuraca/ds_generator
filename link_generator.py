0effd4c56c8e45c885a350d0acfeff8bf025f2a5
stefano <s.misuraca@globalscanning.com>
2020-01-20 13:42:45 +0100

    Link generator v1.0

---
+++ link_generator.py
@@ -0,0 +1,82 @@
+"""
+File description.
+
+Dreamsub.stream is an anime website
+that collects series from various fansub organizations
+and make them available for download.
+
+Their download system is based on a cdn and they generate their links
+based on a simple pattern, quite naive imho.
+
+Therefor a scraper is not needed
+and the download links for the series
+can be automatically generated from the inital url
+"""
+
+import argparse
+import re
+import sys
+
+
+class Dreamsub:
+    """Class definition."""
+
+    split_pattern = "https://dreamsub.stream/anime/"
+    cdn_uri = "https://cdn1.dreamsub.stream/fl/"
+    # https://cdn1.dreamsub.stream/fl/kaguya-sama-wa-kokurasetai-tensai-tachi-no-renai-zunousen/01/SUB_ITA/720p
+
+    def __init__(self, link, episode_number, quality):
+        """Class init."""
+        self.quality = quality
+        self.link = link
+        self.ep_number = int(episode_number)
+
+    def link_generator(self):
+        """Generate links for given series link."""
+        anime_name = self.link.split(self.split_pattern)[1]  # take last part of the uri (the slug) #noqa
+        print(anime_name)
+        for episode in range(1, self.ep_number + 1):
+            full_name = "{cdn}{name}/{ep_number}/SUB_ITA/{quality}p".format(
+                cdn=self.cdn_uri,
+                name=anime_name,
+                ep_number=f"0{episode}" if episode < 10 else episode, #noqa
+                quality=self.quality
+            )
+            # print(full_name)
+            yield full_name
+
+    def list_links(self):
+        """Returns a list of downloadable links for given series."""
+        return list(self.link_generator())
+
+
+parser = argparse.ArgumentParser(description='Dreamsub links generator.')
+parser.add_argument(
+    'link', metavar='https://dreamsub.stream/anime/angel-beats',
+    type=str, help='Link of the series to be generated'
+)
+parser.add_argument(
+    '-e', '--episodes',
+    required=True, dest="episodes",
+    metavar='episodes_number', type=int,
+    help='Number of episodes'
+)
+parser.add_argument(
+    '-q', '--quality', nargs="?", default="720",
+    dest="quality", required=True,
+    metavar='720', type=int,
+    help='An integer for quality selection'
+)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    series_link = args.link
+    episode_number = args.episodes
+    quality = args.quality
+    dreamsub = Dreamsub(
+        link=series_link,
+        episode_number=episode_number,
+        quality=quality)
+
+    print(*dreamsub.list_links(), sep="\n")
